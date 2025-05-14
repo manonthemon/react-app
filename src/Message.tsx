c70a2ea576352e71db9b4bde832b188d1a549989
Manonthemon <klimowicz.maciej@gmail.com>
2025-05-14 12:13:33 +0100

    Dynamically changing message

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,7 +1,11 @@
+// This is a simple React component that returns a header element with the text "Hello {User Name} or {World}!".
+
 function Message() {
     //JSX: JS XML
+  const name = "Maciek";
+  if (name)
+        return <h1>Hello {name}!</h1>;
     return <h1>Hello World!</h1>;
 }
 
 export default Message;
-// This is a simple React component that returns a header element with the text "Hello World!".
