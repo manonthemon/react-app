8169537479efbe9c17875e516433a7247cbab1e6
Manonthemon <klimowicz.maciej@gmail.com>
2025-05-14 12:06:09 +0100

    Created 1st component

---
+++ src/Message.tsx
@@ -0,0 +1,7 @@
+function Message() {
+    //JSX: JS XML
+    return <h1>Hello World!</h1>;
+}
+
+export default Message;
+// This is a simple React component that returns a header element with the text "Hello World!".
