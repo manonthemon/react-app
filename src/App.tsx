4951bf19d71b5b36855cd9d2f74cc92e47ec0276
Manonthemon <klimowicz.maciej@gmail.com>
2025-05-14 17:26:05 +0100

    Added some conditional logic to message display

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import Message from './Message';
-
+import ListGroup from "./components/ListGroup";
 
 function App() {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  // items = [];
+
   return (
-    <div><Message /></div>
+    <>
+      <h1>List Group</h1>
+      
+      {items.length === 0 && <p>NO ITEMS!!!! NOOOO!!!!!</p>}
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
   );
 }
 
-export default App
+export default App;
