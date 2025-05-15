0ba113ceb7943d4f02fc6f7154188d65b0eeebff
Manonthemon <klimowicz.maciej@gmail.com>
2025-05-15 13:38:49 +0100

    Added state handling and renamed files

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,8 @@
 import ListGroup from "./components/ListGroup";
-import { MouseEvent } from "react"; 
 
 function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-
-  //Event handler
-  const handleClick = (e: MouseEvent) => console.log(e);
-
-  return (
-    <>
-      <h1>List Group</h1>
-      
-      {items.length === 0 && <p>NO ITEMS!!!! NOOOO!!!!!</p>}
-      <ul className="list-group">
-        {items.map((item, index) => (
-          <li className= "list-group-item" key={item} onClick={handleClick}>
-            
-            {item}</li>
-        ))}
-      </ul>
-    </>
-  );
+   return <div><ListGroup /></div>; 
 }
 
 export default App;
+
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,5 +1,34 @@
+
+import { useState } from "react";
+
 function ListGroup() {
-   return <h1>List Group</h1>; 
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  //Hooks are functions that let you “hook into” React state and lifecycle features from function components.
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  return (
+    <>
+      <h1>List Group</h1>
+
+      {items.length === 0 && <p>NO ITEMS!!!! NOOOO!!!!!</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {setSelectedIndex(index)}}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default ListGroup;
