f7e8e8df32de81f927e9d2e8fe5dce8ae655745f
Manonthemon <klimowicz.maciej@gmail.com>
2025-05-15 13:47:23 +0100

    Added propos

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,15 +1,20 @@
 
 import { useState } from "react";
 
-function ListGroup() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+interface Props {
+   items:string[];
+   heading: string;
+}
 
-  //Hooks are functions that let you “hook into” React state and lifecycle features from function components.
+function ListGroup({items, heading}: Props) {
+
+
+
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
-      <h1>List Group</h1>
+      <h1>{heading}</h1>
 
       {items.length === 0 && <p>NO ITEMS!!!! NOOOO!!!!!</p>}
       <ul className="list-group">
